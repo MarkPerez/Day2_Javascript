ebc8f9e078e14f095fdbc44665ef850a6ab718a4
MarkPerez <markperez88@hotmail.co.uk>
2016-06-14 10:37:45 +0100

    Exercise 2

--- Javascript.js
+++ Javascript.js
@@ -1,7 +1,14 @@
+//Event Listener
+var event1 = document.getElementById("button1");
+event1.addEventListener('click',threeSum,false);
 
+function threeSum() {
+    var userinput1 = parseInt(document.getElementById("input1").value);
+    var userinput2 = parseInt(document.getElementById("input2").value);
+    var userinput3 = parseInt(document.getElementById("input3").value);
 
-window.alert("Hello World");
-
-
+    document.getElementById("demo").innerHTML = "The sum of the numbers is: " + (userinput1 + userinput2 + userinput3);
+    
+}
 
 
