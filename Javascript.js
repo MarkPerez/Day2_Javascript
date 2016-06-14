7156555d54d003d9113b95cf3ab43ab2410c13d6
MarkPerez <markperez88@hotmail.co.uk>
2016-06-14 11:24:05 +0100

    Example 3

--- Javascript.js
+++ Javascript.js
@@ -1,14 +1,12 @@
 //Event Listener
 var event1 = document.getElementById("button1");
-event1.addEventListener('click',threeSum,false);
+event1.addEventListener('click',activate,false);
 
-function threeSum() {
-    var userinput1 = parseInt(document.getElementById("input1").value);
-    var userinput2 = parseInt(document.getElementById("input2").value);
-    var userinput3 = parseInt(document.getElementById("input3").value);
+var scope = 5
 
-    document.getElementById("demo").innerHTML = "The sum of the numbers is: " + (userinput1 + userinput2 + userinput3);
+function activate() {
+    var scope = parseInt(document.getElementById("input1").value);
+
+    document.getElementById("demo").innerHTML = "The outer scope number is: " + window.scope + "<br>" + "The inner scope number is: " + scope;
     
 }
-
-
