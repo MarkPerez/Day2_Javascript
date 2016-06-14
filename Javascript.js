56da29dc3fd98d7fdd1e89624ea44ff07230f0b6
MarkPerez <markperez88@hotmail.co.uk>
2016-06-14 14:05:58 +0100

    Example 6

--- Javascript.js
+++ Javascript.js
@@ -1,55 +1,15 @@
 //Event Listener
-var event1 = document.getElementById("button1");
-event1.addEventListener('click', whatIsTheDay, false);
 
-var today = new Date();
+window.addEventListener('load', activate, false);
 
-function whatIsTheDay(){
- 
-    var day = "";
-        
-    switch (today.getDay()) {
-        case 0:
-           day = "Sunday"
-            break;
-            
-        case 1:
-           day = "Monday"
-             break;
-            
-        case 2:
-           day = "Tuesday"
-             break;
-             
-        case 3:
-           day = "Wednesday"
-             break;
-        
-        case 4:
-           day = "Thursday"
-             break;
-            
-        case 5:
-           day = "Friday"
-             break;
-             
-        case 6:
-           day = "Saturday"
-             break; 
- 
+var text = "";
+var counter = 1;
+function activate(){
+    while (counter <= 10) {
+        text += "The number is " + counter + "<br>";
+        counter++;
     }
- 
-    document.getElementById("demo").innerHTML = "The day is " + day;
-}
-
-var event2 = document.getElementById("button2");
-event2.addEventListener('click', whatIsTheTime, false);
-    
-function whatIsTheTime(){
-    
-    var time = today.toLocaleTimeString();
-    
-    document.getElementById("demo").innerHTML = "The time is " + time;
+    document.getElementById("demo").innerHTML = text;
 
 }
 
