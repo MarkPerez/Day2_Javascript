9777481a95724b093127fc4934ff1c0d55de4255
MarkPerez <markperez88@hotmail.co.uk>
2016-06-14 14:57:58 +0100

    Exercise 8

--- Javascript.js
+++ Javascript.js
@@ -1,16 +1,27 @@
 //Event Listener
-
 window.addEventListener('load', activate, false);
 
-var text = "";
-var counter = 1;
 function activate(){
-    do {
-        text += "The number is " + counter + "<br>";
-        counter++;
-    } 
-    while (counter <= 10);
-    document.getElementById("demo").innerHTML = text;
+
+//set for loop text
+    var text1 = "";
+
+    //outer for loop
+    for (var i = 0; i < 10; i++) {
+        
+    text1 += "Outer number: " + (i+1) + ": " + "<br>" + "<br>"; 
+        
+        //inner for loop
+        for (var j = 0; j < 10; j++){
+            
+            text1 += "Inner number: " + (j+1) + "<br>" + "<br>";
+            
+        } 
+   
+    }
+    
+    //print to HTML
+    document.getElementById("demo").innerHTML = text1;
 
 }
 
